64660706639f0d6f3f4cbd712551abf02841ea4a
xinzi2018 <dln2014@126.com>
2023-12-19 15:49:25 +0800

    test2

--- helloworld.py
+++ helloworld.py
@@ -1,24 +1,24 @@
 
 
 
-# import numpy as np
-# import os
-# import cv2
+import numpy as np
+import os
+import cv2
 
-# def main():
-#     print('====myhelloworld=====')
-# if __name__ == '__main__':
-#     main()
+def main():
+    print('====myhelloworld=====')
+if __name__ == '__main__':
+    main()
 
 
-from flask import Flask
+# from flask import Flask
  
-server = Flask(__name__)
+# server = Flask(__name__)
  
-@server.route("/")
-def hello():
-  return "Hello World!"
+# @server.route("/")
+# def hello():
+#   return "Hello World!"
  
-if __name__ == "__main__":
-  server.run(host='0.0.0.0')
+# if __name__ == "__main__":
+#   server.run(host='0.0.0.0')
  

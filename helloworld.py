7cab153840c31615af2e1bdf68a0a6418ddd4095
xinzi2018 <dln2014@126.com>
2023-12-22 17:52:42 +0800

    flask1

--- helloworld.py
+++ helloworld.py
@@ -18,11 +18,11 @@ server = Flask(__name__)
  
 @server.route("/helloworld/", methods=['POST'])
 def hello():
-    img_path = request.form['img_path']
+    # img_path = request.form['img_path']
 
      
-    return "Hello World!"+img_path
-    # return "Hello World!" 
+    # return "Hello World!"+img_path
+    return "Hello World!" 
  
 if __name__ == "__main__":
     server.run(host='0.0.0.0',port=1111)

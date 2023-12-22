533137e17c851ddd5fc1161a68fdbe0b3209f5da
xinzi2018 <dln2014@126.com>
2023-12-22 17:44:04 +0800

    flask

--- helloworld.py
+++ helloworld.py
@@ -1,24 +1,29 @@
 
 
 
-import numpy as np
-import os
-import cv2
+# import numpy as np
+# import os
+# import cv2
 
-def main():
-    print('====myhelloworld=====')
-if __name__ == '__main__':
-    main()
+# def main():
+#     print('====myhelloworld=====')
+# if __name__ == '__main__':
+#     main()
 
 
-# from flask import Flask
+from flask import Flask
+from flask import Flask, request
  
-# server = Flask(__name__)
+server = Flask(__name__)
  
-# @server.route("/")
-# def hello():
-#   return "Hello World!"
+@server.route("/helloworld/", methods=['POST'])
+def hello():
+    img_path = request.form['img_path']
+
+     
+    return "Hello World!"+img_path
+    # return "Hello World!" 
  
-# if __name__ == "__main__":
-#   server.run(host='0.0.0.0')
+if __name__ == "__main__":
+    server.run(host='0.0.0.0',port=1111)
  

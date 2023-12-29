cdf58cdf69c9a5f61dfacab09207ceb077fe718e
xinzi2018 <dln2014@126.com>
2023-12-29 16:11:37 +0800

    miaohui1_robot_up

--- trailGenerate/miaohui1_robot.py
+++ trailGenerate/miaohui1_robot.py
@@ -9,45 +9,52 @@ import sys
 
 miaohui = MiaoHui1116()
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
  
-    miaohui = MiaoHui1116()
+#     miaohui = MiaoHui1116()
     
      
-    outputPath ,_= miaohui.trail_generate('./testData/Photo1_output.png', './testData/Photo1_parsing.npy', 
-                                            './testData/Photo1_temp.txt', './testData/Photo1_trail_temp.png', "8ha8azthmj", '000')# error 报错
+#     outputPath ,_= miaohui.trail_generate('./testData/Photo1_output.png', './testData/Photo1_parsing.npy', 
+#                                             './testData/Photo1_temp.txt', './testData/Photo1_trail_temp.png', "8ha8azthmj", '000')# error 报错
 
-    print(outputPath,time.asctime( time.localtime(time.time()) ))
+#     print(outputPath,time.asctime( time.localtime(time.time()) ))
+
+
+ 
+
+
+
+
+from flask import Flask
+from flask import Flask, request
+ 
+server = Flask(__name__)  
+ 
+@server.route("/trail_generate/", methods=['POST'])
+def trail_generate():
+
+
+
+    # sketch_path = request.form['sketch_path']
+    # parsing_path = request.form['parsing_path']
+    # trail_txt_path = request.form['trail_txt_path']
+    # trail_png_path = request.form['trail_png_path']
+    sketch_path = './testData/Photo1_output.png'
+    parsing_path = './testData/Photo1_parsing.npy'
+    trail_txt_path = './testData/Photo1_temp.txt'
+    trail_png_path = './testData/Photo1_trail_temp.png'
     
- 
 
 
-
-
-# from flask import Flask
-# from flask import Flask, request
- 
-# server = Flask(__name__)  
- 
-# @server.route("/trail_generate/", methods=['POST'])
-# def trail_generate():
-
-
-
-#     sketch_path = request.form['sketch_path']
-#     parsing_path = request.form['parsing_path']
-#     trail_txt_path = request.form['trail_txt_path']
-#     trail_png_path = request.form['trail_png_path']
-
-#     outputPath ,_= miaohui.trail_generate(sketch_path, parsing_path, trail_txt_path, trail_png_path, "8ha8azthmj", '000')# error 报错
+    outputPath ,_= miaohui.trail_generate(sketch_path, parsing_path, trail_txt_path, trail_png_path, "8ha8azthmj", '000')# error 报错
  
 
 
      
-#     return  outputPath 
+    return  outputPath 
  
-# if __name__ == "__main__":
-#     server.run(host='0.0.0.0',port=1111)
+if __name__ == "__main__":
+    server.run(host='0.0.0.0',port=8088)
 
 
 
